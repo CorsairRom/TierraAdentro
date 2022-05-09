4e1c5fc2fdf09f4e2296201b2f3af4587398055e
Blanca <bl.hidalgo@duocuc.cl>
2022-05-09 10:08:59 -0400

    no tomar en consideración

--- js/contacto.js
+++ js/contacto.js
@@ -1,16 +1,16 @@
 $().ready(function() {
-	$("#formcontacto").validate({
+	$("#formcontac").validate({
 		rules: {
-			name: { required: true, minlength: 3 ,maxlength: 50 },
+			name: { required:true, minlength: 3 ,maxlength: 50 },
 			email: { required:true, email: true},
 			titulo: { required: true, minlength: 2 },
 			message: { required:true, minlength: 2, maxlength: 500 }
 		},
 		messages: {
-			name: "El campo es obligatorio.",
-			email : "El campo es obligatorio y debe tener formato de email correcto.",
-			titulo : "El campo es obligatorio.",
-			message : "El campo Mensaje es obligatorio"
+			name: "El campo no puede estar vacio.",
+			email : "El campo no puede estar vacio.",
+			titulo : "El campo no puede estar vacio.",
+			message : "El campo no puede estar vacio"
 		}
 	});
 });
@@ -18,15 +18,13 @@ $().ready(function() {
 
 
 $("#guardar").click(function() {
-	if($("#formcontacto").valid() == false){
+	if($("#formcontac").valid() == false){
         return;
     }
     let name = $("#name").val()
     let email = $("#email").val()
     let titulo = $("#titulo").val()
     let message = $("#message").val()
-
-    
 
 });
 
--- js/valuser.js
+++ js/valuser.js
@@ -17,3 +17,5 @@ const datosusuario = function(){
     }
     
 }
+
+
