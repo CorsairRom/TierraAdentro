285295bbf0f7427e8a8e4c17ae2a9311c7e64ded
Blanca <bl.hidalgo@duocuc.cl>
2022-06-13 10:43:11 -0400

    contact_quienes_somos
    mapa y quienes somos

--- js/map.js
+++ js/map.js
@@ -1,11 +1,19 @@
-function iniciarMap() {
-    var coord = { lat: -36.7953523, lng: -73.0636798 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: coord
-    });
-    var marker = new google.maps.Marker({
-        position: coord,
-        map: map
+  // Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -36.7955288, lng: -73.0635351 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(
+    document.getElementById("map") ,
+    {
+        zoom: 17,
+        center: uluru,
+    }
+    );
+
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
     });
 }
